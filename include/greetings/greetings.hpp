679216b4ddd9db6e5a6a5e5e66fc521e1c60f360
Mandimby RAVELOARINJAKA <mandimby.raveloarinjaka@gmail.com>
2012-02-16 11:23:10 -0500

    Initial upload.

---
+++ include/greetings/greetings.hpp
@@ -0,0 +1,13 @@
+#ifndef GREETINGS_HPP
+#define GREETINGS_HPP
+
+#include <string>
+
+namespace murex {
+namespace tests {
+
+std::string greet();
+
+} } // murex::tests
+
+#endif
---
+++ src/greetings/greetings.cpp
@@ -0,0 +1,14 @@
+#include <greetings/greetings.hpp>
+
+#include <string>
+
+namespace murex {
+namespace tests {
+
+std::string greet()
+{
+   return "Hello!";
+}
+
+} } // murex::tests
+
