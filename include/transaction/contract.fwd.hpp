41b2b3670ab36160a0317e404459531b4f447647
mraveloarinjaka <mraveloarinjaka@murex.com>
2012-02-20 15:23:41 -0500

    Base interface.

---
+++ include/transaction/contract.fwd.hpp
@@ -0,0 +1,13 @@
+#ifndef TRANSACTION_CONTRACT_FWD_HPP
+#define TRANSACTION_CONTRACT_FWD_HPP
+
+namespace tests {
+namespace transaction {
+
+class Contract;
+const int CONTRACT_SOURCE_LEN = 11;
+
+} } // tests::transaction
+
+#endif // TRANSACTION_CONTRACT_FWD_HPP
+
