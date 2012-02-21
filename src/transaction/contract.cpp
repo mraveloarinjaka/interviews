62c2e5fdafa82d503f4854aa4c82d5e07f51e318
Mandimby RAVELOARINJAKA <mandimby.raveloarinjaka@gmail.com>
2012-02-21 00:15:23 -0500

    Contracts can now be saved in the database.

--- src/transaction/contract.cpp
+++ src/transaction/contract.cpp
@@ -1,13 +1,149 @@
+/**
+ * =====================================================================================
+ *
+ *       Filename:  contract.cpp
+ *
+ *    Description:  Class defining a contractual agreement between a source and a party
+ *
+ * =====================================================================================
+ */
+
 #include <transaction/contract.hpp>
 
+#include <cstdlib>
 #include <cstring>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 #include <sqlite/sqlite3.h>
 
 namespace tests { namespace transaction {
 
-Contract::Contract()
+namespace {
+
+const int UNIQUE_ID = 1000;
+
+int getUniqueID()
 {
+   static int uniqueId = UNIQUE_ID;
+
+   return uniqueId++;
+}
+
+/** 
+ * Executes a request
+ * Executes a SQL request on a open database
+ * @param db The database connection
+ * @param szRequest The SQL request
+ * @param callback 
+ *   The callback to process the results set 
+ *   (optional) Defaults to 0
+ * @param data 
+ *   The user data passed to the callback
+ *   (optional) Defautls to 0
+ */
+
+typedef int (*ResultsSetCB)(void *, int , char **, char **);
+   
+bool executeRequest(sqlite3 *db, char const *szRequest, ResultsSetCB callback = 0, void *data=0)
+{
+   bool sqlErrorRaised = false;
+   char *szErrMsg = 0;
+   int errorCode = 0;
+
+   errorCode = sqlite3_exec(db, szRequest, callback, data, &szErrMsg);
+   if(SQLITE_OK != errorCode)
+   {
+      sqlErrorRaised = true;
+      std::cerr <<  "SQL error: " << szErrMsg << std::endl;
+      sqlite3_free(szErrMsg);
+   }
+   return sqlErrorRaised;
+}
+
+
+std::string createSaveRequest(Contract const &contract)
+{
+   std::ostringstream request;
+
+   request << "insert into CONTRACTS_DBF values (" << contract.getReference() << ", " 
+           << "'" << contract.getSource()  << "', "
+           << "'" << contract.getParty() << "');" << std::endl;
+
+   return request.str();
+}
+
+std::string createLoadAllRequest()
+{
+   std::ostringstream request;
+
+   request << "select M_REFERENCE, M_SOURCE, M_PARTY from CONTRACTS_DBF";
+
+   return request.str();
+}
+
+enum ContractColumn
+{
+   ContractColumnReference = 0,
+   ContractColumnSource,
+   ContractColumnParty,
+   ContractColumn_
+};
+
+bool readAttribute(ContractColumn column, char *szAttribute, Contract *contract)
+{
+   bool errorRaised = false;
+
+   switch(column)
+   {
+      case ContractColumnReference:
+      {
+         contract->setReference(std::atof(szAttribute));
+         break;
+      }
+      case ContractColumnSource:
+      {
+         contract->setSource(szAttribute);
+         break;
+      }
+      case ContractColumnParty:
+      {
+         contract->setParty(szAttribute);
+         break;
+      }
+      default:
+      {
+         errorRaised = true; // unknown column
+      }
+
+   }
+   return errorRaised;
+}
+
+int loadContract(void *pvContracts, int argc, char **argv, char **szColumNames)
+{
+   bool errorRaised = false;
+   Contracts *contracts = static_cast<Contracts *>(pvContracts);
+   Contract contract;
+
+   for(int i=0; i < argc && !errorRaised; ++i)
+   {
+      errorRaised = readAttribute(static_cast<ContractColumn>(i), argv[i], &contract);
+   }
+   if(!errorRaised)
+   {
+      contracts->push_back(contract);
+   }
+   return errorRaised ? 1 : 0;
+}
+
+
+} // namespace
+ 
+Contract::Contract() : m_reference(0)
+{
+   m_source[0] = 0;
 }
 
 double Contract::getReference() const
@@ -18,6 +154,11 @@ double Contract::getReference() const
 char const *Contract::getSource() const
 {
    return m_source;
+}
+
+char const *Contract::getParty() const
+{
+   return m_party;
 }
 
 
@@ -41,10 +182,52 @@ Contract &Contract::setSource(char const *source)
    }
    return *this;
 }
- 
+
+Contract &Contract::setParty(char const *party)
+{
+   if(party)
+   {
+      strncpy(m_party, party, CONTRACT_PARTY_LEN - 1);
+      m_party[CONTRACT_PARTY_LEN - 1] = 0;
+   }
+   else
+   {
+      m_party[0] = 0;
+   }
+   return *this;
+}
+
 bool Contract::save(sqlite3 *db)
 {
-   return false;
+   bool errorRaised = NULL == db;
+
+   if(!errorRaised)
+   {
+      if(m_reference <= 0)
+      {
+         m_reference = getUniqueID();
+      }
+      errorRaised = executeRequest(db, createSaveRequest(*this).c_str());
+   }
+   return errorRaised;
+}
+
+bool Contract::loadAll(sqlite3 *db, Contracts *contracts)
+{
+   bool errorRaised = NULL == db || NULL == contracts;
+
+   if(!errorRaised)
+   {
+      errorRaised = executeRequest(db, createLoadAllRequest().c_str(), loadContract, contracts);
+   }
+   return errorRaised;
+}
+
+void Contract::toStream(std::ostream &result) const
+{
+   result << "Contract (reference: " << m_reference 
+          << ", source: " << m_source 
+          << ", party: " << m_party << ")" << std::endl;
 }
 
 
